83a45e072a8ca73df80df4edd8a53096bdb8a92e
RCH010 <36447625+RCH010@users.noreply.github.com>
2018-02-19 22:43:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 
 using namespace std;
 #include "Instruccion.h"
-
+//Declaramos 
 int revisaCodigoOperacion(string scop);
-int revisaTipoDireccionamiento (string stipDir);
-int revisaDireccionDato (string sdirDat);
+int revisaTipoDireccionamiento (string stipDir, int cop);
+int revisaDireccionDato (string sdirDat, int cop);
 
+void caseCodigoOperacion (int codigoOperacion);
+//Variables Globales
 bool errorInstruccion = false;
+//AQUI va la variable globar de arreglo de integers de 6 x 1000  que es la MainMemory
 
 int main()
 {
@@ -28,16 +31,26 @@ int main()
     cout<<"Finalmente, escribe la direccion o dato"<<endl;
     cout<<"\tDato-> +00 Direccion-> 100"<<endl;
     cout<<"Ejemplo: LDA A 200 \t en caso de que la direccion sea 2, se debe ingresar 002"<<endl;
-    //input
-    cin>>scop>>stipDir>>sdirDat;
-    //Llamamos a estas funciones para verificar y obtener todo en integers
-    icop = revisaCodigoOperacion(scop);
-    itipDir = revisaTipoDireccionamiento(stipDir);
-    iDatoDireccion = revisaDireccionDato (sdirDat);
+    cout<<"En caso de ser HLT, NOP o CLA, poner dos caracteres despues de esta instruccion"<<endl;
+    cout<<"Ejemplo: HLT 0 0 "<<endl;
 
 
-    if (errorInstruccion)
-        cout<<"Error de Intsruccion"<<endl;
+//AQUI ES DONDE INICIARA EL CICLO, QUE SE DETENDRA HASTA QUE PONGA UN HLT!
+    do {
+        //input
+        cin>>scop>>stipDir>>sdirDat;
+
+        //Llamamos a estas funciones para verificar y obtener todo en integers
+        icop = revisaCodigoOperacion(scop);
+        itipDir = revisaTipoDireccionamiento(stipDir, icop);
+        iDatoDireccion = revisaDireccionDato (sdirDat, icop);
+
+
+        if (errorInstruccion)
+            cout<<"Error de Intsruccion, intente de nuevo"<<endl;
+        errorInstruccion = false;
+
+    } while (scop != "HLT");
 
     return 0;
 }
@@ -69,11 +82,13 @@ int revisaCodigoOperacion(string scop)
 //REVISA EL STRING QUE RECIBE COMO TIPO DE DIRECCIONAMIENTO
 //EN CASO DE QUE NO SEA NINGUNO DE LOS POSIBLES
 //MANDA UN MENSAJE DE ERROR A PANTALLA Y ACTIVA UN BOOL (errorInstruccion)
-int revisaTipoDireccionamiento (string stipDir)
+int revisaTipoDireccionamiento (string stipDir, int cop)
 {
     int itipoDir = -1;
 
-    if(stipDir == "INM") { itipoDir = 0; }
+    if ((cop == 0) || (cop == 1) || (cop == 10))
+        return itipoDir;
+    else if(stipDir == "INM") { itipoDir = 0; }
     else if (stipDir == "R") { itipoDir = 1; }
     else if (stipDir == "A") { itipoDir = 2; }
     else if (stipDir == "IND") { itipoDir = 3; }
@@ -88,13 +103,15 @@ int revisaTipoDireccionamiento (string stipDir)
 //REVISA QUE LO INGRESADO SEA DEL TAMAÑO PERMITIDO
 //REVISA SI ES DATO O DIRECCION, ACTIVANDO UN BOOL EN SU CASO (dato)
 //Y DESPUES LO CONVIERTE A INTEGER PARA PODER USARLO MAS ADELANTE
-int revisaDireccionDato (string sdirDat)
+int revisaDireccionDato (string sdirDat, int cop)
 {
     int valor;
     bool dato = false;
     bool negativo = false;
 
-    if(sdirDat.length() > 3)
+    if ((cop == 0) || (cop == 1) || (cop == 10))
+        return valor;
+    else if(sdirDat.length() > 3)
         errorInstruccion = true;
     else if ((sdirDat[0] == '+') || (sdirDat[0] == '-'))
     {
@@ -122,3 +139,35 @@ int revisaDireccionDato (string sdirDat)
     return valor;
 }
 
+
+void caseCodigoOperacion (int codigoOperacion)
+{
+    switch (codigoOperacion)
+    {
+    case 0:
+        //Llamar funcion NOP
+        break;
+    case 1:
+        //Llamar funcion CLA
+        break;
+    case 2:
+        //Llamar funcion NEG
+        break;
+    case 3:
+        //Llamar funcion LDA
+        break;
+    case 4:
+        //Llamar funcion STA
+        break;
+    case 5:
+        //LLamar funcion ADD
+        break;
+    case 6:
+        //LLamar funcion SUB
+        break;
+    case 10:
+        //Llamar funcion HLT
+        break;
+
+    }
+}
